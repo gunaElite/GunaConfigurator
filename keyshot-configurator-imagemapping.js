8f75829622393a2248316860f226b805ac67eae4
gunaElite <guna.m@eliteelevators.com>
2023-04-20 16:16:51 +0530

    Guna Code added sucessfuly

---
+++ keyshot-configurator-imagemapping.js
@@ -0,0 +1,201 @@
+imagemappingdata =
+[
+    {
+        "image": "Gunaconfig3.bip.1.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+8",
+            "15+12"
+        ],
+        "studio": 579216862
+    },
+    {
+        "image": "Gunaconfig3.bip.2.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+9",
+            "15+12"
+        ],
+        "studio": 579216862
+    },
+    {
+        "image": "Gunaconfig3.bip.3.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+10",
+            "15+12"
+        ],
+        "studio": 579216862
+    },
+    {
+        "image": "Gunaconfig3.bip.4.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+8",
+            "15+13"
+        ],
+        "studio": 579216862
+    },
+    {
+        "image": "Gunaconfig3.bip.5.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+9",
+            "15+13"
+        ],
+        "studio": 579216862
+    },
+    {
+        "image": "Gunaconfig3.bip.6.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+10",
+            "15+13"
+        ],
+        "studio": 579216862
+    },
+    {
+        "image": "Gunaconfig3.bip.7.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+8",
+            "15+14"
+        ],
+        "studio": 579216862
+    },
+    {
+        "image": "Gunaconfig3.bip.8.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+9",
+            "15+14"
+        ],
+        "studio": 579216862
+    },
+    {
+        "image": "Gunaconfig3.bip.9.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+10",
+            "15+14"
+        ],
+        "studio": 579216862
+    },
+    {
+        "image": "Gunaconfig3.bip.10.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+8",
+            "15+12"
+        ],
+        "studio": 485884556
+    },
+    {
+        "image": "Gunaconfig3.bip.11.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+9",
+            "15+12"
+        ],
+        "studio": 485884556
+    },
+    {
+        "image": "Gunaconfig3.bip.12.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+10",
+            "15+12"
+        ],
+        "studio": 485884556
+    },
+    {
+        "image": "Gunaconfig3.bip.13.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+8",
+            "15+13"
+        ],
+        "studio": 485884556
+    },
+    {
+        "image": "Gunaconfig3.bip.14.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+9",
+            "15+13"
+        ],
+        "studio": 485884556
+    },
+    {
+        "image": "Gunaconfig3.bip.15.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+10",
+            "15+13"
+        ],
+        "studio": 485884556
+    },
+    {
+        "image": "Gunaconfig3.bip.16.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+8",
+            "15+14"
+        ],
+        "studio": 485884556
+    },
+    {
+        "image": "Gunaconfig3.bip.17.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+9",
+            "15+14"
+        ],
+        "studio": 485884556
+    },
+    {
+        "image": "Gunaconfig3.bip.18.jpg",
+        "modelsets": [
+            611713997
+        ],
+        "multimaterials": [
+            "11+10",
+            "15+14"
+        ],
+        "studio": 485884556
+    }
+];
---
+++ keyshot-configurator-modeldata.js
@@ -0,0 +1,84 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "taupe%20beige.png",
+                    "multimaterials": [
+                        "15+12"
+                    ],
+                    "name": "Taupe beige"
+                },
+                {
+                    "icon": "scarlet%20red.png",
+                    "multimaterials": [
+                        "15+13"
+                    ],
+                    "name": "Scarlet Red"
+                },
+                {
+                    "checked": true,
+                    "icon": "ashen%20grey.png",
+                    "multimaterials": [
+                        "15+14"
+                    ],
+                    "name": "Ashen Grey"
+                }
+            ],
+            "name": "carpet Color"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "material%20set%201%201.png",
+                    "multimaterials": [
+                        "11+8"
+                    ],
+                    "name": "Material set 1 1"
+                },
+                {
+                    "icon": "material%20set%201%202.png",
+                    "multimaterials": [
+                        "11+9"
+                    ],
+                    "name": "Material set 1 2"
+                },
+                {
+                    "checked": true,
+                    "icon": "material%20set%201%203.png",
+                    "multimaterials": [
+                        "11+10"
+                    ],
+                    "name": "Material set 1 3"
+                }
+            ],
+            "name": "Material set 1"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "fullmodel.png",
+            "materialgroups": [
+                "carpet Color",
+                "Material set 1"
+            ],
+            "modelset": 611713997,
+            "name": "FullModel"
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "lift%20full%2090mm%20fl.png",
+            "id": 579216862,
+            "name": "Lift Full 90mm FL"
+        },
+        {
+            "icon": "carpet%20color.png",
+            "id": 485884556,
+            "name": "carpet color"
+        }
+    ]
+};
